1e4722a3c7ca9e2c0986dfce734a0ebee7f8c674
Matic Leva <vertical3life@gmail.com>
2018-03-09 22:46:43 +0100

    Add simple player sprite and objects with colliders

---
+++ Assets/Data/CharacterStats.cs
@@ -0,0 +1,25 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class CharacterStats : ScriptableObject {
+
+    public float speed;
+
+    [MenuItem("Assets/Create/Last Link/Character stats")]
+    public static void CreateCharacterStats()
+    {
+        string path = EditorUtility.SaveFilePanelInProject("Create Character stats", "Character stats", "asset", "Create Character stats", "Assets");
+
+        if (string.IsNullOrEmpty(path))
+            return;
+
+        CharacterStats asset = CreateInstance<CharacterStats>();
+
+        AssetDatabase.CreateAsset(asset, path);
+        AssetDatabase.SaveAssets();
+
+        EditorUtility.FocusProjectWindow();
+
+        Selection.activeObject = asset;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    CharacterStats stats;
+    Rigidbody2D rigidBody;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        if (!stats)
+            Debug.LogError("Assign character stats");
+    }
+
+    void Update()
+    {
+        Vector2 direction = Vector2.zero;
+        if (Input.GetKey(KeyCode.W))
+            direction += Vector2.up;
+        if (Input.GetKey(KeyCode.S))
+            direction += Vector2.down;
+        if (Input.GetKey(KeyCode.A))
+            direction += Vector2.left;
+        if (Input.GetKey(KeyCode.D))
+            direction += Vector2.right;
+
+        rigidBody.MovePosition((Vector2)transform.position + direction * Time.deltaTime * stats.speed);
+    }
+}
